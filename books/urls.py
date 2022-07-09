b176f6320bb29b5c3a697568c88d029b0c385c0d
Krishnanunni R <krishnanunnir97@gmail.com>
2022-07-09 12:56:31 +0530

    added sort for html

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.BookView.as_view(), name="index"),
-    path("<int:pk>", views.BookSeriesView.as_view(), name="series"),
+    path("books/", views.BookView.as_view(), name="index"),
+    path("series/<int:pk>", views.BookSeriesView.as_view(), name="series"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views import View
 from django.views.generic.detail import DetailView
+from django.views.generic.base import TemplateView
 from books.models import Book, BookSeries
 import logging
 from django.db.models import Q
@@ -8,8 +8,16 @@ from django.db.models import Q
 logger = logging.getLogger(__name__)
 
 
-class BookView(View):
-    def get(self, request):
+class BookView(TemplateView):
+    template_name = "home.html"
+
+    def get_template_names(self):
+        if self.request.GET.get("order"):
+            return "_books.html"
+        return "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(BookView, self).get_context_data(**kwargs)
         can_be_public = Q(
             amazon_link__isnull=False,
             goodreads_link__isnull=False,
@@ -17,13 +25,26 @@ class BookView(View):
             name__isnull=False,
             author__isnull=False,
         )
+        order = self.request.GET.get("order")
         books = Book.objects.filter(can_be_public, book_number=1)
-        return render(request, "home.html", {"books": books})
+        if order:
+            try:
+                books = books.order_by(f"-{order}")
+            except Exception:
+                logger.error(f"Couldn't sort for-{order}")
+
+        context["books"] = books
+        return context
 
 
 class BookSeriesView(DetailView):
     model = BookSeries
     template_name = "home.html"
+
+    def get_template_names(self):
+        if self.request.GET.get("order"):
+            return "_books.html"
+        return "home.html"
 
     def get_context_data(self, **kwargs):
         context = super(BookSeriesView, self).get_context_data(**kwargs)
@@ -35,6 +56,12 @@ class BookSeriesView(DetailView):
             author__isnull=False,
         )
         books = Book.objects.filter(can_be_public, series=self.kwargs.get("pk"))
+        order = self.request.GET.get("order")
+        if order:
+            try:
+                books = books.order_by(f"-{order}")
+            except Exception:
+                logger.error(f"Couldn't sort for-{order}")
         if books:
             context["books"] = books
         return context
