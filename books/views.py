da65711857571ac547ea66c8ebe7fff3cc913e4e
Krishnanunni R <krishnanunnir97@gmail.com>
2022-07-09 09:35:09 +0530

    View condition to decide which show

--- books/views.py
+++ books/views.py
@@ -3,11 +3,19 @@ from django.views import View
 
 from books.models import Book
 import logging
+from django.db.models import Q
 
 logger = logging.getLogger(__name__)
 
 
 class BookView(View):
     def get(self, request):
-        books = Book.objects.all()
+        can_be_public = Q(
+            amazon_link__isnull=False,
+            goodreads_link__isnull=False,
+            description__isnull=False,
+            name__isnull=False,
+            author__isnull=False,
+        )
+        books = Book.objects.filter(can_be_public, book_number=1)
         return render(request, "home.html", {"books": books})
