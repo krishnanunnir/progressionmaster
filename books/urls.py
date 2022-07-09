fef5fb09b86ea9b74378639554a16c1e25237b9c
Krishnanunni R <krishnanunnir97@gmail.com>
2022-07-09 20:25:16 +0530

    minor cleanups and adding about.html

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("books/", views.BookView.as_view(), name="index"),
+    path("", views.BookView.as_view(), name="index"),
     path("series/<int:pk>", views.BookSeriesView.as_view(), name="series"),
+    path("about/", TemplateView.as_view(template_name="about.html")),
 ]
