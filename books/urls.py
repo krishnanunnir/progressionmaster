970ae70911cb351b6a3bddad234ed11096515b71
Krishnanunni R <krishnanunnir97@gmail.com>
2022-07-09 10:09:03 +0530

    added page for all books of a series

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.BookView.as_view(), name="index"),
+    path("<int:pk>", views.BookSeriesView.as_view(), name="series"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-
-from books.models import Book
+from django.views.generic.detail import DetailView
+from books.models import Book, BookSeries
 import logging
 from django.db.models import Q
 
@@ -19,3 +19,22 @@ class BookView(View):
         )
         books = Book.objects.filter(can_be_public, book_number=1)
         return render(request, "home.html", {"books": books})
+
+
+class BookSeriesView(DetailView):
+    model = BookSeries
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(BookSeriesView, self).get_context_data(**kwargs)
+        can_be_public = Q(
+            amazon_link__isnull=False,
+            goodreads_link__isnull=False,
+            description__isnull=False,
+            name__isnull=False,
+            author__isnull=False,
+        )
+        books = Book.objects.filter(can_be_public, series=self.kwargs.get("pk"))
+        if books:
+            context["books"] = books
+        return context
